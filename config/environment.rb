9c78c895ab36d201e829898c4c562d020a120a11
Wonjin Cho <cwonjin11@gmail.com>
2021-03-26 10:47:59 -0400

    done

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,13 @@ Bundler.require
 
 
 # put the code to connect to the database here
+ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/artists.sqlite"
+  ) 
+  #This file is requiring the gems in our Gemfile and giving our program access to them. 
+  #We're using the establish_connection method from ActiveRecord::Base to connect to our artists database, 
+  #which will be created in the migration via SQLite3 (the adapter).
 
 
 require_relative "../artist.rb"
